65f3826e845b35cdf1d82834a7b57800a09f771a
Xinlin Liu <apps_store@126.com>
2022-06-06 13:44:14 +0800

    Add files via upload

---
+++ XXXSDK.framework/Headers/HelloWorld.h
@@ -0,0 +1,18 @@
+//
+//  HelloWorld.h
+//  XXXSDK
+//
+//  Created by lxl on 2022/6/6.
+//
+
+#import <Foundation/Foundation.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface HelloWorld : NSObject
+
++ (NSString *)getString;
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ XXXSDK.framework/Headers/XXXSDK.h
@@ -0,0 +1,18 @@
+//
+//  XXXSDK.h
+//  XXXSDK
+//
+//  Created by lxl on 2022/6/6.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for XXXSDK.
+FOUNDATION_EXPORT double XXXSDKVersionNumber;
+
+//! Project version string for XXXSDK.
+FOUNDATION_EXPORT const unsigned char XXXSDKVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <XXXSDK/PublicHeader.h>
+
+#import <XXXSDK/HelloWorld.h>
