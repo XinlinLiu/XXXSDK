d732f9a1bcd119a1a6791c749eb9e3628087b554
Xinlin Liu <apps_store@126.com>
2022-08-17 10:34:23 +0800

    Add files via upload

---
+++ BundleSDK.framework/Headers/BundleHome.h
@@ -0,0 +1,18 @@
+//
+//  BundleHome.h
+//  BundleSDK
+//
+//  Created by lxl on 2022/7/22.
+//
+
+#import <UIKit/UIKit.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface BundleHome : UIView
+
++ (void)log;
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ BundleSDK.framework/Headers/BundleSDK.h
@@ -0,0 +1,18 @@
+//
+//  BundleSDK.h
+//  BundleSDK
+//
+//  Created by lxl on 2022/7/22.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for BundleSDK.
+FOUNDATION_EXPORT double BundleSDKVersionNumber;
+
+//! Project version string for BundleSDK.
+FOUNDATION_EXPORT const unsigned char BundleSDKVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <BundleSDK/PublicHeader.h>
+
+#import <BundleSDK/BundleHome.h>
